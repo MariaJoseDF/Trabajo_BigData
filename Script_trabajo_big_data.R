fe855665caf450cf7a7832ad8b51e14a57295fd7
MariaJoseDF <maria.donosof@utem.cl>
2020-12-30 09:14:11 -0300

    Perfeccionando la extracción de los datos
    Se perfecciona la tabla que contiene los datos extraidos mediante su orden, eliminación de descripciones basura y la transfromacion a valores numericos de variados datos

--- Script_trabajo_big_data.R
+++ Script_trabajo_big_data.R
@@ -1,10 +1,12 @@
-#setwd("C:/Users/maria/OneDrive/Escritorio/ICI Cuarto año/Segundo semestre/Big data/Trabajo_BigData")
+setwd("C:/Users/maria/OneDrive/Escritorio/ICI Cuarto año/Segundo semestre/Big data/Trabajo_BigData")
 
-#Instalando paquete
+#Instalando paquetes
 install.packages("rvest")
 library("rvest")
 install.packages("dplyr")
 library("dplyr")
+install.packages("gdata")
+library("gdata")
 
 #Lectura de pagina con la informacion requerida
 ig <- read_html("https://www.ig.com/es/trading-de-criptomonedas/comparativa-criptomonedas#information-banner-dismiss")
@@ -12,39 +14,77 @@ ig <- read_html("https://www.ig.com/es/trading-de-criptomonedas/comparativa-crip
 #Extrayendo tabla comparativa de criptomonedas
 Tabla_comparativa <- html_table(ig)[[2]]
 
+#Cambiando filas por columnas
+Tabla_comparativa <- as.data.frame(t(Tabla_comparativa))
+
 #Renombrando columnas
 
-Tabla_comparativa <- rename(Tabla_comparativa, "Bitcoin (BTC)" = X2, "Bitcoin cash (BCH)" = X3, "Ether (ETH)" = X4, "Litecoin (LTC)" = X5,
-                            "Ripple (XRP)" = X6, "EOS (EOS)" = X7, "Stellar (XLM)" = X8, "NEO (NEO)" = X9, "Característica" = X1)
-
-#Creando vector con los nombres de las filas
-Filas <- list("Lanzamiento (año)", "Cantidad en circulación (M)", "Oferta máxima (M)", "Ratio de minado/emisión", "Transacciones por segundo (máximo)",
-           "Red", "Tiempo para un bloque (aprox.)")
+Tabla_comparativa <- rename(Tabla_comparativa, "Criptomoneda" = V1, "Lanzamiento" = V2, "Cantidad en circulación (>M)" = V3,
+                            "Oferta máxima (M)" = V4, "Ratio de minado/emisión" = V5, "Transacciones por segundo" = V6,
+                            "Red" = V7, "Tiempo para un bloque (segundos)" = V8)
 
 #Eliminando fila sobrante
 Tabla_comparativa <- Tabla_comparativa[-1,]
 
+#Creando vector con los nombres de las filas
+Filas <- list(1,2,3,4,5,6,7,8)
+
 #Reasignando los nombres de las filas
 Tabla_comparativa <- data.frame(Tabla_comparativa, row.names = Filas)
-
-#Renombrando las columnas debido a error ocurrido al cambiar filas
-Tabla_comparativa <- rename(Tabla_comparativa,"Bitcoin (BTC)" = Bitcoin..BTC., "Bitcoin cash (BCH)" = Bitcoin.cash..BCH., "Ether (ETH)" = Ether..ETH., "Litecoin (LTC)" = Litecoin..LTC.,
-                            "Ripple (XRP)" = Ripple..XRP., "EOS (EOS)" = EOS..EOS., "Stellar (XLM)" = Stellar..XLM., "NEO (NEO)" = NEO..NEO.)
-
-#Eliminando columna sobrante
-Tabla_comparativa <- Tabla_comparativa[,-1]
 
 #Estableciendo loop que reemplazara por NA las variables sin datos
 
 for (i in 1:nrow(Tabla_comparativa)) {
-  if(Tabla_comparativa[6,i] == "n/a"){
-    Tabla_comparativa[6,i] <- NA
+  if(Tabla_comparativa[i,7] == "n/a"){
+    Tabla_comparativa[i,7] <- NA
   }
 }
 
-#Transformando variables a valores numericos
-Tabla_comparativa[1,] <- as.numeric(Tabla_comparativa[1,])
-Tabla_comparativa[2,] <- gsub("millones","",Tabla_comparativa[2,])
-Tabla_comparativa[3,] <- gsub("millones","",Tabla_comparativa[3,])
-Tabla_comparativa[3,] <- gsub("(preminados)","",Tabla_comparativa[3,])
-Tabla_comparativa[3,] <- gsub("[()]","",Tabla_comparativa[3,])
+#Transformando el tiempo de la columna 8 en minutos por segundos y a valores numericos
+Tabla_comparativa[1,8] <- 10*60
+Tabla_comparativa[2,8] <- 10*60
+Tabla_comparativa[3,8] <- 15
+Tabla_comparativa[4,8] <- 2.5 * 60
+Tabla_comparativa[6,8] <- 0.5
+Tabla_comparativa[7,8] <- 5
+Tabla_comparativa[8,8] <- 15
+Tabla_comparativa[5,8] <- 0.1
+Tabla_comparativa[,8] <- as.numeric(Tabla_comparativa[,8])
+
+#Tranformando la columna 6 a valores numericos
+Tabla_comparativa[,6] <- gsub("[.]","",Tabla_comparativa[,6])
+Tabla_comparativa[,6] <- as.numeric(Tabla_comparativa[,6])
+
+#Transformando las variables de la columna 3
+Tabla_comparativa[,3] <- gsub("[>]","",Tabla_comparativa[,3])
+Tabla_comparativa[,3] <- gsub("millones","",Tabla_comparativa[,3])
+Tabla_comparativa[,3] <- gsub("[.]","",Tabla_comparativa[,3])
+Tabla_comparativa[,3] <- as.numeric(Tabla_comparativa[,3])
+
+#Transformando las variables de la columna 2
+Tabla_comparativa[,2] <- as.numeric(Tabla_comparativa[,2])
+
+#Transformando las variables de la columna 1
+Tabla_comparativa[,1] <- gsub("1","",Tabla_comparativa[,1])
+Tabla_comparativa[,1] <- gsub("2","",Tabla_comparativa[,1])
+Tabla_comparativa[,1] <- gsub("3","",Tabla_comparativa[,1])
+Tabla_comparativa[,1] <- gsub("4","",Tabla_comparativa[,1])
+Tabla_comparativa[,1] <- gsub("5","",Tabla_comparativa[,1])
+
+#Reemplazando las variables de la columna 4 que indican sin limite por NA
+for (i in 1:nrow(Tabla_comparativa)) {
+  if(Tabla_comparativa[i,4] == "Sin límite máximo"){
+    Tabla_comparativa[i,4] <- NA
+  }
+}
+#Transformando la columna 4 a valores numericos
+Tabla_comparativa[,4] <- gsub("millones","",Tabla_comparativa[,4])
+Tabla_comparativa[,4] <- gsub("(preminados)","",Tabla_comparativa[,4])
+Tabla_comparativa[,4] <- gsub("[()]","",Tabla_comparativa[,4])
+Tabla_comparativa[,4] <- gsub("[.]","",Tabla_comparativa[,4])
+Tabla_comparativa[,4] <- as.numeric(Tabla_comparativa[,4])
+
+#Corrigiendo error en la sintaxis de las columnas
+Tabla_comparativa <- rename(Tabla_comparativa, "Cantidad en circulación (>M)" = Cantidad.en.circulación...M., 
+                            "Oferta máxima (M)" = Oferta.máxima..M., "Ratio de minado/emisión" = Ratio.de.minado.emisión,
+                            "Transacciones por sg" = Transacciones.por.segundo, "Tiempo para un bloque (sg)" = Tiempo.para.un.bloque..segundos.)
